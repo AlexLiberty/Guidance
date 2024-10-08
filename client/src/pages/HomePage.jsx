87abe85c88e0cc4dd12224aab7a97289d0ca7e4f
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-08 17:42:03 +0300

    complete task Layout+Header

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <p className="text-6xl font-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">this is HomePage</p>;
+}
--- client/src/pages/IndexPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function IndexPage() {
-  return <p>this is Index page</p>;
-}
